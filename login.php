cc4922bcff268465646c5a78c8fd5c62410b92d1
Abdulmosinkhanpathan <114325519+Abdulmosinkhanpathan@users.noreply.github.com>
2023-04-21 14:28:30 +0530

    Update and rename index.php to login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost"; // replace with your server name
+$username = $_POST["system"]; // get the username from the form
+$password = $_POST["mosin2003"]; // get the password from the form
+$dbname = "localhost"; // replace with your database name
+
+// Create connection
+$conn = oci_connect($username, $password, "//".$servername."/".$dbname);
+
+// Check connection
+if (!$conn) {
+    $error_message = oci_error();
+    echo "Login failed: " . htmlentities($error_message['message']);
+} else {
+    // Connection successful
+    echo "Hello";
+}
+
+// Close connection
+oci_close($conn);
+?>
